c460faf9f7c97f4859e242fada74c6a6e14f10ef
Jeremy Goldman <jeremygoldman@fullsail.edu>
2013-04-13 21:39:42 -0500

    Sections created, and styles added

--- js/json.js
+++ js/json.js
@@ -8,7 +8,7 @@ var json = {
 	"reminder1": {
 		"group": ["Group:", "Personal"],
 		"remindTitle" : ["Reminder Title:", "Lunch"],
-		"dueDate"	: ["Due Date:", "2013-03-29"],
+		"dueDate"	: ["Due Date:", "2013-04-29"],
 		"recurrence" : ["Recurrence:", "10"],
 		"description" : ["Description:", "Test Data 1"]
 		
@@ -23,12 +23,154 @@ var json = {
 	},
 	"reminder3": {
 		"group": ["Group:", "Other"],
-		"remindTitle" : ["Reminder Title:", "Complete VFW"],
-		"dueDate"	: ["Due Date:", "2013-03-28"],
+		"remindTitle" : ["Reminder Title:", "Homework"],
+		"dueDate"	: ["Due Date:", "2013-04-28"],
 		"recurrence" : ["Recurrence:", "1"],
 		"description" : ["Description:", "Test Data 3"]
+		"reminder1": {
+		"group": ["Group:", "Personal"],
+		"remindTitle" : ["Reminder Title:", "Lunch"],
+		"dueDate"	: ["Due Date:", "2013-04-29"],
+		"recurrence" : ["Recurrence:", "10"],
+		"description" : ["Description:", "Test Data 1"]
 		
-	}
+	},
+	"reminder4": {
+		"group": ["Group:", "Work"],
+		"remindTitle" : ["Reminder Title:", "Sales Report"],
+		"dueDate"	: ["Due Date:", "2013-04-01"],
+		"recurrence" : ["Recurrence:", "8"],
+		"description" : ["Description:", "Test Data 2"]
+		
+	},
+	"reminder5": {
+		"group": ["Group:", "Other"],
+		"remindTitle" : ["Reminder Title:", "Homework"],
+		"dueDate"	: ["Due Date:", "2013-04-28"],
+		"recurrence" : ["Recurrence:", "1"],
+		"description" : ["Description:", "Test Data 3"]
+	},
+	"reminder6": {
+		"group": ["Group:", "Personal"],
+		"remindTitle" : ["Reminder Title:", "Lunch"],
+		"dueDate"	: ["Due Date:", "2013-04-29"],
+		"recurrence" : ["Recurrence:", "10"],
+		"description" : ["Description:", "Test Data 1"]
+		
+	},
+	"reminder7": {
+		"group": ["Group:", "Work"],
+		"remindTitle" : ["Reminder Title:", "Sales Report"],
+		"dueDate"	: ["Due Date:", "2013-04-01"],
+		"recurrence" : ["Recurrence:", "8"],
+		"description" : ["Description:", "Test Data 2"]
+		
+	},
+	"reminder8": {
+		"group": ["Group:", "Other"],
+		"remindTitle" : ["Reminder Title:", "Homework"],
+		"dueDate"	: ["Due Date:", "2013-04-28"],
+		"recurrence" : ["Recurrence:", "1"],
+		"description" : ["Description:", "Test Data 3"]]
+	},
+
+	"reminder9": {
+		"group": ["Group:", "Personal"],
+		"remindTitle" : ["Reminder Title:", "Lunch"],
+		"dueDate"	: ["Due Date:", "2013-04-29"],
+		"recurrence" : ["Recurrence:", "10"],
+		"description" : ["Description:", "Test Data 1"]
+		
+	},
+	"reminder10": {
+		"group": ["Group:", "Work"],
+		"remindTitle" : ["Reminder Title:", "Sales Report"],
+		"dueDate"	: ["Due Date:", "2013-04-01"],
+		"recurrence" : ["Recurrence:", "8"],
+		"description" : ["Description:", "Test Data 2"]
+		
+	},
+	"reminder11": {
+		"group": ["Group:", "Other"],
+		"remindTitle" : ["Reminder Title:", "Homework"],
+		"dueDate"	: ["Due Date:", "2013-04-28"],
+		"recurrence" : ["Recurrence:", "1"],
+		"description" : ["Description:", "Test Data 3"]
+	},
+	"reminder12": {
+		"group": ["Group:", "Personal"],
+		"remindTitle" : ["Reminder Title:", "Lunch"],
+		"dueDate"	: ["Due Date:", "2013-04-29"],
+		"recurrence" : ["Recurrence:", "10"],
+		"description" : ["Description:", "Test Data 1"]
+		
+	},
+	"reminder13": {
+		"group": ["Group:", "Work"],
+		"remindTitle" : ["Reminder Title:", "Sales Report"],
+		"dueDate"	: ["Due Date:", "2013-04-01"],
+		"recurrence" : ["Recurrence:", "8"],
+		"description" : ["Description:", "Test Data 2"]
+		
+	},
+	"reminder14": {
+		"group": ["Group:", "Other"],
+		"remindTitle" : ["Reminder Title:", "Homework"],
+		"dueDate"	: ["Due Date:", "2013-04-28"],
+		"recurrence" : ["Recurrence:", "1"],
+		"description" : ["Description:", "Test Data 3"]
+
+		
+	},
+	"reminder15": {
+		"group": ["Group:", "Personal"],
+		"remindTitle" : ["Reminder Title:", "Lunch"],
+		"dueDate"	: ["Due Date:", "2013-04-29"],
+		"recurrence" : ["Recurrence:", "10"],
+		"description" : ["Description:", "Test Data 1"]
+		
+	},
+	"reminder16": {
+		"group": ["Group:", "Work"],
+		"remindTitle" : ["Reminder Title:", "Sales Report"],
+		"dueDate"	: ["Due Date:", "2013-04-01"],
+		"recurrence" : ["Recurrence:", "8"],
+		"description" : ["Description:", "Test Data 2"]
+		
+	},
+	"reminder17": {
+		"group": ["Group:", "Other"],
+		"remindTitle" : ["Reminder Title:", "Homework"],
+		"dueDate"	: ["Due Date:", "2013-04-28"],
+		"recurrence" : ["Recurrence:", "1"],
+		"description" : ["Description:", "Test Data 3"]
+	},
+	"reminder18": {
+		"group": ["Group:", "Personal"],
+		"remindTitle" : ["Reminder Title:", "Lunch"],
+		"dueDate"	: ["Due Date:", "2013-04-29"],
+		"recurrence" : ["Recurrence:", "10"],
+		"description" : ["Description:", "Test Data 1"]
+		
+	},
+	"reminder19": {
+		"group": ["Group:", "Work"],
+		"remindTitle" : ["Reminder Title:", "Sales Report"],
+		"dueDate"	: ["Due Date:", "2013-04-01"],
+		"recurrence" : ["Recurrence:", "8"],
+		"description" : ["Description:", "Test Data 2"]
+		
+	},
+	"reminder20": {
+		"group": ["Group:", "Other"],
+		"remindTitle" : ["Reminder Title:", "Homework"],
+		"dueDate"	: ["Due Date:", "2013-04-28"],
+		"recurrence" : ["Recurrence:", "1"],
+		"description" : ["Description:", "Test Data 3"]
+
+		
+	},
+
 	
 	
 }
